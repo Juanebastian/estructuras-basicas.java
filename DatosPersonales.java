cd77c52a5b1fbe976597fc93d6fd87f85aaf0dd7
JUAN SEBASTIAN DIAZ CHAPARRO <juansebastiandiazchaparro27@gmail.com>
2025-04-24 23:26:32 -0500

    adicion de la clase DatosPersonales

---
+++ DatosPersonales.java
@@ -0,0 +1,16 @@
+public class DatosPersonales {
+    public static void main(String[] args) {
+        // Declaración de variables
+        String nombre = "Juan Sebastian Diaz Chaparro";
+        int edad = 24;
+        double altura = 1.65;
+        boolean esEstudiante = true;
+
+        // Mostrar los datos
+        System.out.println("---- DATOS PERSONALES ----");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("Altura: " + altura + " metros");
+        System.out.println("¿Es estudiante?: " + esEstudiante);
+    }
+}
